e6c8ebdb8da1aa6b05f9cc16ad1b9ef97d2081a6
Sama Mazzawi <sama.mazz93@gmail.com>
2023-04-20 20:21:02 +0300

    removed data section

--- sol.s
+++ sol.s
@@ -2,18 +2,6 @@
 	.intel_syntax noprefix
 	.def	___main;	.scl	2;	.type	32;	.endef
 	.section .rdata,"dr"
-LC0:
-	.ascii "\12\0"
-LC1:
-	.ascii "road\0"
-LC2:
-	.ascii "settlement\0"
-LC3:
-	.ascii "city\0"
-LC4:
-	.ascii "development\0"
-LC5:
-	.ascii "%02d \0"
 	.text
 	.globl	_main
 	.def	_main;	.scl	2;	.type	32;	.endef
@@ -64,7 +52,7 @@ LFB13:
                         call FindFunction   # call FindFunction("GetProcAddress")
                         add esp, 0x14       # clear stack
                         pop ebx             # restore module handle for msvcrt
-                        mov     DWORD PTR [ebp+120], eax # initialize get_proc_addr_ptr
+                        mov     DWORD PTR [esp+120], eax # initialize get_proc_addr_ptr
 
                         # PRINTF
                         push 0x00006674		# pushing null,f,t
@@ -74,7 +62,7 @@ LFB13:
 
                         call eax            # call GetProcAddress(msvcrt, "printf")
                         add esp, 0x08       # clear stack (note arguments are cleared already)
-                        mov     DWORD PTR [ebp+104], eax # initialize printf_ptr
+                        mov     DWORD PTR [esp+104], eax # initialize printf_ptr
 
                         # STRCMP
                         push 0x0000706D		# pushing null,p,m
@@ -82,10 +70,10 @@ LFB13:
                         push esp            # push pointer for "strcmp"
                         push ebx            # push module handle for msvcrt
 
-                        mov eax, DWORD PTR [ebp+120] # get_proc_addr_ptr
+                        mov eax, DWORD PTR [esp+120] # get_proc_addr_ptr
                         call eax            # call GetProcAddress(msvcrt, "strcmp")
                         add esp, 0x08       # clear stack (note arguments are cleared already)
-                        mov     DWORD PTR [ebp+108], eax # initialize strcmp_ptr
+                        mov     DWORD PTR [esp+108], eax # initialize strcmp_ptr
 
                         # strcspn
                         push 0x006E7073		# pushing null,n,p,s
@@ -93,10 +81,10 @@ LFB13:
                         push esp            # push pointer for "strcspn"
                         push ebx            # push module handle for msvcrt
 
-                        mov eax, DWORD PTR [ebp+120] # get_proc_addr_ptr
+                        mov eax, DWORD PTR [esp+120] # get_proc_addr_ptr
                         call eax            # call GetProcAddress(msvcrt, "strcmp")
                         add esp, 0x08       # clear stack (note arguments are cleared already)
-                        mov     DWORD PTR [ebp+116], eax # initialize strcspn_ptr
+                        mov     DWORD PTR [esp+116], eax # initialize strcspn_ptr
 
                         # fgets
                         push 0x00000073		# pushing null,s
@@ -104,10 +92,10 @@ LFB13:
                         push esp            # push pointer for "fgets"
                         push ebx            # push module handle for msvcrt
 
-                        mov eax, DWORD PTR [ebp+120] # get_proc_addr_ptr
+                        mov eax, DWORD PTR [esp+120] # get_proc_addr_ptr
                         call eax            # call GetProcAddress(msvcrt, "strcmp")
                         add esp, 0x08       # clear stack (note arguments are cleared already)
-                        mov     DWORD PTR [ebp+112], eax # initialize fgets_ptr
+                        mov     DWORD PTR [esp+112], eax # initialize fgets_ptr
 
                         # putchar
                         push 0x00726168		# pushing null,r,a,h
@@ -115,13 +103,10 @@ LFB13:
                         push esp            # push pointer for "putchar"
                         push ebx            # push module handle for msvcrt
 
-                        mov eax, DWORD PTR [ebp+120] # get_proc_addr_ptr
+                        mov eax, DWORD PTR [esp+120] # get_proc_addr_ptr
                         call eax            # call GetProcAddress(msvcrt, "strcmp")
                         add esp, 0x08       # clear stack (note arguments are cleared already)
-                        mov     DWORD PTR [ebp+100], eax # initialize putchar
-
-
-
+                        mov     DWORD PTR [esp+100], eax # initialize putchar
 
 
 
@@ -133,17 +118,22 @@ LFB13:
 	mov	DWORD PTR [esp+44], 0
 	jmp	L2
 L6:
-	mov	DWORD PTR [esp+4], OFFSET FLAT:LC0
+	push 0x0032315C		# pushing \,1,2,\0
 	lea	eax, [esp+50]
-	mov	DWORD PTR [esp], eax
-	mov eax, DWORD PTR [ebp+116]
+	push eax
+	mov eax, DWORD PTR [esp+116]
 	call	eax # strcspn
+	sub esp, 8
+
+
 	mov	BYTE PTR [esp+50+eax], 0
-	mov	DWORD PTR [esp+4], OFFSET FLAT:LC1
+	push 0x00000000		# null
+	push 0x64616F72		# d,a,o,r
 	lea	eax, [esp+50]
-	mov	DWORD PTR [esp], eax
-	mov eax, DWORD PTR [ebp+108]
+	push eax
+	mov eax, DWORD PTR [esp+108]
 	call	eax # strcmp
+	sub esp, 12
 	test	eax, eax
 	jne	L3
 	mov	eax, DWORD PTR [esp+28]
@@ -153,11 +143,14 @@ L6:
 	add	eax, 1
 	mov	DWORD PTR [esp+32], eax
 L3:
-	mov	DWORD PTR [esp+4], OFFSET FLAT:LC2
+	push 0x0000746E # null,t,n
+	push 0x656D656C # e,m,e,l
+	push 0x74746573 # t,t,e,s
 	lea	eax, [esp+50]
-	mov	DWORD PTR [esp], eax
-	mov eax, DWORD PTR [ebp+108]
+	push eax
+	mov eax, DWORD PTR [esp+108]
 	call	eax # strcmp
+	sub esp, 16
 	test	eax, eax
 	jne	L4
 	mov	eax, DWORD PTR [esp+28]
@@ -173,11 +166,13 @@ L3:
 	add	eax, 1
 	mov	DWORD PTR [esp+40], eax
 L4:
-	mov	DWORD PTR [esp+4], OFFSET FLAT:LC3
+	push 0x00000000 # null
+	push 0x79746963 # y,t,i,c
 	lea	eax, [esp+50]
-	mov	DWORD PTR [esp], eax
-	mov eax, DWORD PTR [ebp+108]
+	push eax
+	mov eax, DWORD PTR [esp+108]
 	call	eax # strcmp
+	sub esp, 12
 	test	eax, eax
 	jne	L5
 	mov	eax, DWORD PTR [esp+40]
@@ -187,11 +182,14 @@ L4:
 	add	eax, 3
 	mov	DWORD PTR [esp+44], eax
 L5:
-	mov	DWORD PTR [esp+4], OFFSET FLAT:LC4
+	push 0x00746E65 # null,t,n,e
+	push 0x6D706F6C # m,p,o,l
+	push 0x65766564 # e,v,e,d
 	lea	eax, [esp+50]
-	mov	DWORD PTR [esp], eax
-	mov eax, DWORD PTR [ebp+108]
+	push eax
+	mov eax, DWORD PTR [esp+108]
 	call	eax # strcmp
+	sub esp, 16
 	test	eax, eax
 	jne	L2
 	mov	eax, DWORD PTR [esp+36]
@@ -209,7 +207,7 @@ L2:
 	mov	DWORD PTR [esp+4], 50
 	lea	eax, [esp+50]
 	mov	DWORD PTR [esp], eax
-	mov eax, DWORD PTR [ebp+112]
+	mov eax, DWORD PTR [esp+112]
 	call	eax # fgets
 	test	eax, eax
 	jne	L6
@@ -219,15 +217,17 @@ L8:
 	mov	eax, DWORD PTR [esp+124]
 	mov	eax, DWORD PTR [esp+28+eax*4]
 	mov	DWORD PTR [esp+4], eax
-	mov	DWORD PTR [esp], OFFSET FLAT:LC5
-	mov eax, DWORD PTR [ebp+104]
+	push 0x00000020 # null, space
+	push 0x64323025 # d,2,0,%
+	mov eax, DWORD PTR [esp+104]
 	call	eax # printf
+	sub esp, 8
 	add	DWORD PTR [esp+124], 1
 L7:
 	cmp	DWORD PTR [esp+124], 4
 	jle	L8
 	mov	DWORD PTR [esp], 10
-	mov eax, DWORD PTR [ebp+100]
+	mov eax, DWORD PTR [esp+100]
 	call	eax # putchar
 	mov	eax, 0
 	leave
@@ -235,6 +235,9 @@ L7:
 	.cfi_def_cfa 4, 4
 	ret
 	.cfi_endproc
+
+FindFunction:
+
 LFE13:
 	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
 	.def	_strcspn;	.scl	2;	.type	32;	.endef
@@ -243,5 +246,3 @@ LFE13:
 	.def	_printf;	.scl	2;	.type	32;	.endef
 	.def	_putchar;	.scl	2;	.type	32;	.endef
 
-
-FindFunction:
